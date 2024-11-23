59a78370e7055f1f74c11d9798b067a6811941ab
matsu_web <rm.57.boc@gmail.com>
2024-11-23 16:12:10 +0900

    Active storageを使ってprofile画像をアップロードできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      # &.attached?で画像がアップロードされているか調べる
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end
 end
