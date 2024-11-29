8eb4a5b3f862776438bf82cfb27d17a0bb84643f
matsu_web <rm.57.boc@gmail.com>
2024-11-29 18:07:23 +0900

    いいねに関するjsを別ファイルに分割する

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,39 +20,3 @@ require("channels")
 require("trix");
 require("@rails/actiontext");
 
-import $ from 'jquery'
-import axios from "axios";
-import { csrfToken } from "rails-ujs";
-
-axios.defaults.headers.common["X-CSRF-Token"] = csrfToken();
-
-const handleHeartDisplay = (hasLiked, activeHeart, inactiveHeart) => {
-  if (hasLiked) {
-    activeHeart.classList.remove("hidden");
-    inactiveHeart.classList.add("hidden");
-  } else {
-    activeHeart.classList.add("hidden");
-    inactiveHeart.classList.remove("hidden");
-  }
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  const articles = document.querySelectorAll(".article");
-
-  articles.forEach((article) => {
-    const articleId = article.dataset.articleId;
-    const activeHeart = article.querySelector(".article_body_icon_heart.active-heart");
-    const inactiveHeart = article.querySelector(".article_body_icon_heart.inactive-heart");
-
-    // サーバーからいいね状態を取得
-    axios
-      .get(`/articles/${articleId}/like`)
-      .then((response) => {
-        const hasLiked = response.data.hasLiked;
-        handleHeartDisplay(hasLiked, activeHeart, inactiveHeart);
-      })
-      .catch((error) => {
-        console.error("Error fetching like status:", error);
-      });
-  });
-});
