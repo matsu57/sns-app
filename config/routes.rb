3e9ff86020f95c81da970f5048343a5df572a4f4
matsu_web <rm.57.boc@gmail.com>
2024-12-10 21:25:59 +0900

    ユーザーの詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resource :like, only: [:show, :create, :destroy]
     resources :comments, only: [:index, :new, :create]
   end
+
+  resources :accounts, only: [:show]
 
   resource :profile, only: [:show, :edit, :update]
 end
