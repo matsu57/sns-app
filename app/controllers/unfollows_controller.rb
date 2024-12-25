42c8708ff75c643c3b40de66f43d928b2db62fea
matsu_web <rm.57.boc@gmail.com>
2024-12-25 16:05:32 +0900

    unfollowにはshowが必要ないので削除

--- app/controllers/unfollows_controller.rb
+++ app/controllers/unfollows_controller.rb
@@ -1,12 +1,6 @@
 class UnfollowsController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @user= User.find(params[:account_id])
-    follow_status = current_user.has_followed?(@user)
-    render json: { hasFollowed: follow_status, followersCount: @user.reload.followers.count }
-  end
-
   def create
     current_user.unfollow!(params[:account_id])
     @user= User.find(params[:account_id])
