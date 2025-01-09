2e797f13223981e853ec95b8f6edc571e77bd31c
matsu_web <rm.57.boc@gmail.com>
2025-01-09 20:51:11 +0900

    投稿内容のバリデーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -19,6 +19,7 @@ class Article < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  validates :content, presence: true
   validates :images,
     attached_file_presence: true,
     attached_file_number: { maximum: 4 },
