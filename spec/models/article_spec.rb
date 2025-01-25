9660563dca83521ea10f7480c476573ec8357623
matsu_web <rm.57.boc@gmail.com>
2025-01-25 16:07:10 +0900

    contextを使って前提条件の設定などを行う

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it '内容と画像が入力されていれば、記事を保存できる' do
-    user = User.create!({
-      email: 'test@examlpe.com',
-      password: 'password',
-      username: 'test1'
-    })
-    article = user.articles.build({
-      content: Faker::Lorem.sentence(word_count: 8),
-      # imagesにActiveStorage::Blobオブジェクトを配列で設定
-      images: [ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/login-bg.png')), filename: 'login-bg.png', content_type: 'image/png')] 
-    })
+  context 'タイトルと内容が入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'test@examlpe.com',
+        password: 'password',
+        username: 'test1'
+      })
+      @article = user.articles.build({
+        content: Faker::Lorem.sentence(word_count: 8),
+        # imagesにActiveStorage::Blobオブジェクトを配列で設定
+        images: [ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/login-bg.png')), filename: 'login-bg.png', content_type: 'image/png')] 
+      })
+    end
 
-    expect(article).to be_valid
+    it '記事を保存できる' do
+      expect(@article).to be_valid
+    end
   end
 end
