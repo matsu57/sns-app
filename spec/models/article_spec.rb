fedc0e33f90ebc393857fc1c7bd18ee51391c91b
matsu_web <rm.57.boc@gmail.com>
2025-01-25 17:08:38 +0900

    記事の内容が未入力の時に保存に失敗するか確認する

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  context 'タイトルと内容が入力されている場合' do
-    let!(:user) do
-      User.create!({
-        email: 'test@examlpe.com',
-        password: 'password',
-        username: 'test1'
-      })
-    end
+  let!(:user) do
+    User.create!({
+      email: 'test@examlpe.com',
+      password: 'password',
+      username: 'test1'
+    })
+  end
 
+  context 'タイトルと内容が入力されている場合' do
     let!(:article) do
       user.articles.build({
         content: Faker::Lorem.sentence(word_count: 8),
@@ -17,21 +17,23 @@ RSpec.describe Article, type: :model do
         images: [ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/login-bg.png')), filename: 'login-bg.png', content_type: 'image/png')] 
       })
     end
-    # before do
-    #   user = User.create!({
-    #     email: 'test@examlpe.com',
-    #     password: 'password',
-    #     username: 'test1'
-    #   })
-    #   @article = user.articles.build({
-    #     content: Faker::Lorem.sentence(word_count: 8),
-    #     # imagesにActiveStorage::Blobオブジェクトを配列で設定
-    #     images: [ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/login-bg.png')), filename: 'login-bg.png', content_type: 'image/png')] 
-    #   })
-    # end
 
     it '記事を保存できる' do
-      expect(@article).to be_valid
+      expect(article).to be_valid
+    end
+  end
+
+  context '内容が入力されていない場合' do
+    let!(:article) do
+      user.articles.create({
+        content: '',
+        # imagesにActiveStorage::Blobオブジェクトを配列で設定
+        images: [ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/login-bg.png')), filename: 'login-bg.png', content_type: 'image/png')] 
+      })
+    end
+
+    it '記事を保存できない' do
+      expect(article.errors.messages[:content][0]).to eq("can't be blank")
     end
   end
 end
