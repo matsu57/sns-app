e6df2e8aa9b49f7a9895c29b4f6f2a0261f5850c
matsu_web <rm.57.boc@gmail.com>
2025-01-25 22:36:26 +0900

    factory botを使ってarticleを作成する

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,33 +4,18 @@ RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
-    let(:article) do
-      article = user.articles.build(
-        content: Faker::Lorem.sentence(word_count: 5)
-      )
-      article.images.attach(
-        io: File.open(Rails.root.join('app/assets/images/login-bg.png')),
-        filename: 'login-bg.png'
-      )
-      article
-    end
+    let(:article) { build(:article, user: user) }
 
     it '記事を保存できる' do
-      puts user.username
       expect(article).to be_valid
     end
   end
 
   context '内容が入力されていない場合' do
-    let(:article) do
-      article = user.articles.create(
-        content: ''
-      )
-      article.images.attach(
-        io: File.open(Rails.root.join('app/assets/images/login-bg.png')),
-        filename: 'login-bg.png'
-      )
-      article
+    let(:article) { build(:article, content: '', user: user) }
+
+    before do
+      article.save
     end
 
     it '記事を保存できない' do
