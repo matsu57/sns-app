9765371945a7d424f8c87841b0dd6cf277be2b68
matsu_web <rm.57.boc@gmail.com>
2025-01-25 16:10:26 +0900

    letを使用して書き換える

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,18 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   context 'タイトルと内容が入力されている場合' do
-    before do
-      user = User.create!({
+    let!(:user) do
+      User.create!({
         email: 'test@examlpe.com',
         password: 'password',
         username: 'test1'
       })
-      @article = user.articles.build({
+    end
+
+    let!(:article) do
+      user.articles.build({
         content: Faker::Lorem.sentence(word_count: 8),
         # imagesにActiveStorage::Blobオブジェクトを配列で設定
         images: [ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/login-bg.png')), filename: 'login-bg.png', content_type: 'image/png')] 
       })
     end
+    # before do
+    #   user = User.create!({
+    #     email: 'test@examlpe.com',
+    #     password: 'password',
+    #     username: 'test1'
+    #   })
+    #   @article = user.articles.build({
+    #     content: Faker::Lorem.sentence(word_count: 8),
+    #     # imagesにActiveStorage::Blobオブジェクトを配列で設定
+    #     images: [ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/login-bg.png')), filename: 'login-bg.png', content_type: 'image/png')] 
+    #   })
+    # end
 
     it '記事を保存できる' do
       expect(@article).to be_valid
