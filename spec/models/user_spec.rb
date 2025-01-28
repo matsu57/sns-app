59e81fe484dbb9b2b3331d89e53db0fe6586e8d0
matsu_web <rm.57.boc@gmail.com>
2025-01-28 21:16:09 +0900

    userが保存できない場合のテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,42 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
+
+  context 'emailが入力されていない場合' do
+    let!(:user) { build(:user, email: '') }
+
+    it 'ユーザー情報を保存できない' do
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:email][0]).to include("can't be blank")
+    end
+  end
+
+  context 'usernameが入力されていない場合' do
+    let!(:user) { build(:user, username: '') }
+
+    it 'ユーザー情報を保存できない' do
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:username][0]).to include("can't be blank")
+    end
+  end
+
+  context '同じemailが入力された場合' do
+    let!(:user1) { create(:user, email: 'test@example.com') }
+    let!(:user2) { build(:user, email: 'test@example.com') }
+
+    it 'ユーザー情報を保存できない' do
+      expect(user2).not_to be_valid
+      expect(user2.errors.messages[:email][0]).to include("has already been taken")
+    end
+  end
+
+  context '同じusernameが入力された場合' do
+    let!(:user1) { create(:user, username: 'test_username') }
+    let!(:user2) { build(:user, username: 'test_username') }
+
+    it 'ユーザー情報を保存できない' do
+      expect(user2).not_to be_valid
+      expect(user2.errors.messages[:username][0]).to include("has already been taken")
+    end
+  end
 end
