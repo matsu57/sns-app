3a2e13208e9daa6e31ced6f05bdddfc1c07fd367
matsu_web <rm.57.boc@gmail.com>
2025-01-30 21:37:07 +0900

    間違ったメンションが入力された場合のテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -38,4 +38,26 @@ RSpec.describe Comment, type: :model do
       expect(@comment).to be_valid
     end
   end
+
+  context '間違ったメンション付きのコメントが入力されている場合' do
+    let(:user) { create(:user) }
+    let!(:user2) { create(:user, username: 'recipient_user', email: 'recipient@example.com') }
+    let!(:article) { create(:article, user: user2) }
+
+    before do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it 'コメントは保存されるが、メールが送信されない' do
+      comment_content = "@wrong_username #{Faker::Lorem.sentence(word_count: 5)}"
+
+      expect {
+        @comment = article.comments.create!({
+          content: comment_content,
+          user: user
+        })
+      }.to change { ActionMailer::Base.deliveries.count }.by(0)
+      expect(@comment).to be_valid
+    end
+  end
 end
