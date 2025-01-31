beb3603a129f4ed52e0c600b05bca692447b5e8b
matsu_web <rm.57.boc@gmail.com>
2025-01-31 15:21:28 +0900

    likeが保存されない場合（重複している場合）のテスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Like, type: :model do
       expect(like).to be_valid
     end
   end
+
+  context '同じ記事に同じユーザーがいいねした場合' do
+    let!(:like1) do
+      article.likes.create(user: user)
+    end
+
+    it 'いいねが重複して作成されない' do
+      like2 = build(:like, article: article, user: user)
+      expect(like2).not_to be_valid
+      expect(like2.errors[:user_id]).to include("has already been taken")
+    end
+  end
 end
