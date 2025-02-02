e554f6f6c247f0eed8fce3c719e0ec4e362124d7
matsu_web <rm.57.boc@gmail.com>
2025-02-02 15:10:08 +0900

    likeが重複している場合のテストの修正

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -18,10 +18,14 @@ RSpec.describe Like, type: :model do
     let!(:like1) do
       article.likes.create(user: user)
     end
+    let!(:like2) do
+      article.likes.build(user: user)
+    end
+    before do
+      like2.save
+    end
 
     it 'いいねが重複して作成されない' do
-      like2 = build(:like, article: article, user: user)
-      expect(like2).not_to be_valid
       expect(like2.errors[:user_id]).to include("has already been taken")
     end
   end
