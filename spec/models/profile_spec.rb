0c25af360a335d00a75cd9aa8aed4ac2b466a3d4
matsu_web <rm.57.boc@gmail.com>
2025-02-04 10:56:36 +0900

    profileが保存されない場合のテスト

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe Profile, type: :model do
       expect(profile).to be_valid
     end
   end
+
+  context 'avatar画像が5MB以上のとき' do
+    let!(:profile) do
+      profile = user.build_profile
+      profile.avatar.attach(
+        io: File.open(Rails.root.join('app/assets/images/5MB.png')),
+        filename: '5MB.png',
+        content_type: 'image/png'
+      )
+      profile
+    end
+
+    before do
+      profile.save
+    end
+
+    it 'avatarが保存されない' do
+      expect(profile.errors.messages[:avatar][0]).to eq("must be less than 5 MB")
+    end
+  end
 end
