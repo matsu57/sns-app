1b9652557fda8730005a27e82ad0d6aec0ddcf7f
matsu_web <rm.57.boc@gmail.com>
2025-02-06 22:04:21 +0900

    article_contorollerのPOSTのテスト

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -11,4 +11,28 @@ RSpec.describe 'Articles', type: :request do
     end
   end
 
+  describe 'POST /articles' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+      it '記事が保存されていることを確認する' do
+        article_params = attributes_for(:article)
+        image_file = fixture_file_upload(Rails.root.join('app/assets/images/test1.png'), 'image/png')
+        post articles_path, params: { article: article_params.merge(images: [image_file]) }
+
+        expect(response).to have_http_status(302)
+        expect(Article.last.content).to eq(article_params[:content])
+        expect(Article.last.images).to be_attached
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'ログイン画面に遷移する' do
+        article_params = attributes_for(:article)
+        post articles_path({article: article_params})
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
