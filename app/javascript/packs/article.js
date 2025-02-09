8de52de9b4ae48f74337923c6e164324dd6a4b1a
matsu_web <rm.57.boc@gmail.com>
2025-02-09 11:59:09 +0900

    likeのテスト中にログインしていな場合コンソールにエラーが表示されていることが分かったので修正

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -16,26 +16,32 @@ const handleHeartDisplay = (hasLiked, activeHeart, inactiveHeart) => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  const articles = document.querySelectorAll(".article");
+  const signIn = document.getElementById("sign-in");
+  const isSignIn = signIn.dataset.signinStatus === "true";
 
-  articles.forEach((article) => {
-    const articleId = article.dataset.articleId;
-    const activeHeart = article.querySelector(".article_body_icon_heart.active-heart");
-    const inactiveHeart = article.querySelector(".article_body_icon_heart.inactive-heart");
+  // サインインしている場合のみ動作する
+  if (isSignIn) {
+    const articles = document.querySelectorAll(".article");
 
-    // サーバーからいいね状態を取得
-    axios
-      .get(`/api/articles/${articleId}/like`)
-      .then((response) => {
-        const hasLiked = response.data.hasLiked;
-        const lastLikeUsername = response.data.lastLikeUsername;
-        handleHeartDisplay(hasLiked, activeHeart, inactiveHeart);
-      })
-      .catch((error) => {
-        console.error("Error fetching like status:", error);
-      });
-  });
+    articles.forEach((article) => {
+      const articleId = article.dataset.articleId;
+      const activeHeart = article.querySelector(".article_body_icon_heart.active-heart");
+      const inactiveHeart = article.querySelector(".article_body_icon_heart.inactive-heart");
 
+      // サーバーからいいね状態を取得
+      axios
+        .get(`/api/articles/${articleId}/like`)
+        .then((response) => {
+          const hasLiked = response.data.hasLiked;
+          const lastLikeUsername = response.data.lastLikeUsername;
+          handleHeartDisplay(hasLiked, activeHeart, inactiveHeart);
+        })
+        .catch((error) => {
+          console.error("Error fetching like status:", error);
+        });
+    });
+
+  }
   // オンタイムでのハートのつけ外し
-  heartListener();
+  heartListener(isSignIn);
 });
