fb0a3ddf414f474fe56b5774c130733375ae4963
matsu_web <rm.57.boc@gmail.com>
2025-02-10 21:43:56 +0900

    articles_controllerのPOSTに項目追加、routesの使用していないメソッドを外す

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resource :timeline, only: [:show]
 
-  resources :articles do
+  resources :articles, only: [:index, :new, :show, :create, :destroy] do
     resources :comments, only: [:index]
   end
 
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Articles', type: :request do
       before do
         sign_in user
       end
-      it '記事が保存されていることを確認する' do
+      it '記事が保存される' do
         article_params = attributes_for(:article)
         image_file = fixture_file_upload(Rails.root.join('app/assets/images/test1.png'), 'image/png')
         post articles_path, params: { article: article_params.merge(images: [image_file]) }
@@ -25,12 +25,23 @@ RSpec.describe 'Articles', type: :request do
         expect(Article.last.content).to eq(article_params[:content])
         expect(Article.last.images).to be_attached
       end
+
+      it '無効なパラメータでは記事が保存されない' do
+        invalid_article_params = attributes_for(:article, content: nil)
+        expect {
+          post articles_path, params: { article: invalid_article_params }
+        }.not_to change(Article, :count)
+
+        expect(response).to have_http_status(200) # ステータスコードが200（フォーム再表示）であることを確認
+        expect(flash[:error]).to eq '保存に失敗しました'
+      end
     end
 
     context 'ログインしていない場合' do
       it 'ログイン画面に遷移する' do
         article_params = attributes_for(:article)
         post articles_path({article: article_params})
+        expect(response).to have_http_status(302)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
