cf24b80dfeb26e23ae8b8fba0cbb405f3d53e06a
matsu_web <rm.57.boc@gmail.com>
2025-02-19 21:44:30 +0900

    unfollows_controllerのPOSTのテスト

--- app/controllers/api/v1/unfollows_controller.rb
+++ app/controllers/api/v1/unfollows_controller.rb
@@ -2,8 +2,12 @@ class Api::V1::UnfollowsController < Api::ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.unfollow!(params[:account_id])
     @user= User.find(params[:account_id])
-    render json: { status: 'ok', followersCount: @user.reload.followers.count }
+    if current_user.has_followed?(@user)
+      current_user.unfollow!(params[:account_id])
+      render json: { status: 'ok', followersCount: @user.reload.followers.count }
+    else
+      render json: { error: 'You are not yet following user', followersCount: @user.followers.count }
+    end
   end
 end
---
+++ spec/requests/api/v1/unfollows_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Follows', type: :request do
+  let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:followers) { create_list(:user, 3) }
+  let!(:followings) { create_list(:user, 2) }
+
+  describe 'POST /api/account/:account_id/unfollows' do
+    context 'ログインしていて、other_userがuserをfollowしている場合' do
+      before do
+        sign_in user
+        sign_in other_user
+        other_user.follow!(user)
+      end
+
+      it 'unfollowできる' do
+        post api_unfollows_path(account_id: user.id)
+        body = JSON.parse(response.body)
+        expect(body['status']).to eq('ok')
+        expect(body['followersCount']).to eq(0)
+      end
+    end
+
+    context 'ログインしていて、other_userがuserをfollowしていない場合' do
+      before do
+        sign_in user
+        sign_in other_user
+      end
+
+      it 'unfollowできない(followerの数が変わらない)' do
+        expect {
+          post api_unfollows_path(account_id: user.id)
+        }.not_to change { user.followers.count }
+        body = JSON.parse(response.body)
+        expect(body['error']).to eq('You are not yet following user')
+        expect(body['followersCount']).to eq(0)
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it '401ステータスが返ってくる' do
+        post api_unfollows_path(account_id: user.id)
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
+end
