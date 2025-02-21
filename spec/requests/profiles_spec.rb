4411b165240d3af52f4b72e002168bfa7df1fe46
matsu_web <rm.57.boc@gmail.com>
2025-02-21 15:37:31 +0900

    profiles_controllerのPUTのテスト

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -23,4 +23,31 @@ RSpec.describe "Profiles", type: :request do
       end
     end
   end
+
+  describe 'PUT /profile' do
+    context "ログインしている場合" do
+      before do
+        sign_in user
+        user.prepare_profile  # profileを準備する
+      end
+      it "avatarが更新できる" do
+        expect(user.profile).to be_present
+        avatar_file = fixture_file_upload(Rails.root.join('app/assets/images/test1.png'), 'image/png')
+        put profile_path, params: { profile: { avatar: avatar_file } }
+
+        expect(response).to have_http_status(302)
+        user.profile.reload
+        expect(user.profile.avatar).to be_attached
+        expect(user.profile.avatar.filename.to_s).to eq('test1.png')
+      end
+    end
+
+    context "ログインしていない場合" do
+      it 'ログイン画面に遷移する' do
+        put profile_path
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
