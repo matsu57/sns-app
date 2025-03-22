c4b6b592482b44ad6259d8d47c7f53f2734f4182
matsu_web <rm.57.boc@gmail.com>
2025-03-22 21:08:56 +0900

    profileのエラー内容確認

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,14 +9,27 @@ class ProfilesController < ApplicationController
   def update
     @profile = current_user.prepare_profile
     @profile.assign_attributes(profile_params)
-    if @profile.save
-      flash[:notice] = 'プロフィール更新完了'
-    else
-      flash[:error] = 'プロフィール更新に失敗しました'
-      # エラーメッセージも含める
-      flash[:errors] = @profile.errors.full_messages.join(', ')
+    # if @profile.save
+    #   flash[:notice] = 'プロフィール更新完了'
+    # else
+    #   flash[:error] = 'プロフィール更新に失敗しました'
+    #   # エラーメッセージも含める
+    #   flash[:errors] = @profile.errors.full_messages.join(', ')
+    # end
+    # redirect_to profile_path
+    begin
+      if @profile.save # ここで何らかのエラーが発生していそう
+        redirect_to profile_path, notice: '保存しました'
+      else
+        flash[:error] = 'プロフィール更新に失敗しました'
+        # エラーメッセージも含める
+        flash[:errors] = @profile.errors.full_messages.join(', ')
+      end
+    rescue => e # beginの中でエラーが発生した場合はこちらの処理が実行される
+      Rails.logger.error "Unexpected error in profile creation: #{e.message}"
+      Rails.logger.error "#{e.backtrace.join("\n")}"
+      redirect_to profile_path, alert: '保存中にエラーが発生しました'
     end
-    redirect_to profile_path
   end
 
   private
