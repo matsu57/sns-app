97b2c3ec7de6cedee267946152a32718c9a21860
matsu_web <rm.57.boc@gmail.com>
2025-03-22 10:16:16 +0900

    テストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# john = User.create!(email: 'john@sample.com', password: 'password', username: 'john')
-# emily = User.create!(email: 'emily@sample.com', password: 'password', username: 'emily')
+john = User.create!(email: 'john@sample.com', password: 'password', username: 'john')
+emily = User.create!(email: 'emily@sample.com', password: 'password', username: 'emily')
 
 # 既存のユーザーを取得
-john = User.find_by(username: 'john') # ここで適切な条件でユーザーを検索
-emily = User.find_by(username: 'emily') # ここで適切な条件でユーザーを検索
+# john = User.find_by(username: 'john') # ここで適切な条件でユーザーを検索
+# emily = User.find_by(username: 'emily') # ここで適切な条件でユーザーを検索
 
 # ユーザーが存在する場合のみ記事を作成
 if john
@@ -30,18 +30,18 @@ else
   puts "User 'John' not found."
 end
 
-# if emily
-#   2.times do
-#     article = emily.articles.create!(
-#       content: Faker::Lorem.sentence(word_count: 5)
-#     )
+if emily
+  2.times do
+    article = emily.articles.create!(
+      content: Faker::Lorem.sentence(word_count: 5)
+    )
     
-#     # 画像を記事に添付
-#     article.images.attach(
-#       io: File.open(Rails.root.join('app/assets/images/login-bg.png')),
-#       filename: 'login-bg.png'
-#     )
-#   end
-# else
-#   puts "User 'emily' not found."
-# end
+    # 画像を記事に添付
+    article.images.attach(
+      io: File.open(Rails.root.join('app/assets/images/login-bg.png')),
+      filename: 'login-bg.png'
+    )
+  end
+else
+  puts "User 'emily' not found."
+end
