f7c73f3a64ceacc03adfe86e8d13224c560b133d
matsu_web <rm.57.boc@gmail.com>
2025-03-23 15:22:55 +0900

    profiles_controllerのupdateを修正する

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,48 +6,21 @@ class ProfilesController < ApplicationController
     @profile = current_user.profile
   end
 
-  # def update
-  #   @profile = current_user.prepare_profile
-  #   @profile.assign_attributes(profile_params)
-  #   if @profile.save
-  #     flash[:notice] = 'プロフィール更新完了'
-  #   else
-  #     flash[:error] = 'プロフィール更新に失敗しました'
-  #     エラーメッセージも含める
-  #     flash[:errors] = @profile.errors.full_messages.join(', ')
-  #   end
-  #   redirect_to profile_path
-  # end
-
   def update
     @profile = current_user.prepare_profile
-    begin
-      if @profile.update(profile_params)
-        flash[:notice] = 'プロフィール更新完了'
-        redirect_to profile_path
-      else
-        flash.now[:error] = 'プロフィール更新に失敗しました'
-        flash.now[:errors] = @profile.errors.full_messages.join(', ')
-        render :edit
-      end
-    rescue => e
-      Rails.logger.error "Unexpected error in profile update: #{e.message}"
-      Rails.logger.error "Error class: #{e.class}"
-      Rails.logger.error e.backtrace.join("\n")
-      Rails.logger.debug "Received params: #{params.inspect}"
-      Rails.logger.debug "Permitted params: #{profile_params.inspect}"
-      redirect_to profile_path, alert: '予期せぬエラーが発生しました'
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      flash[:notice] = 'プロフィール更新完了'
+    else
+      flash[:error] = 'プロフィール更新に失敗しました'
+      # エラーメッセージも含める
+      flash[:errors] = @profile.errors.full_messages.join(', ')
     end
+    redirect_to profile_path
   end
 
   private
   def profile_params
     params.require(:profile).permit(:avatar)
   end
-  # def profile_params
-  #   params.require(:profile).permit(:avatar)
-  # rescue ActionController::ParameterMissing
-  #   params.permit(:avatar)
-  # end
-
 end
